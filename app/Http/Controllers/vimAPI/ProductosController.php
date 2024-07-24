48175118cb1bdc3f3fabd20e2f088044c5005a36
devgars <gramirez@mym.com>
2024-07-24 17:49:06 -0500

    create api and controller for linea, marca and origen

--- app/Http/Controllers/vimAPI/ProductosController.php
+++ app/Http/Controllers/vimAPI/ProductosController.php
@@ -5,8 +5,11 @@ namespace App\Http\Controllers\vimAPI;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-use DB;
-// use Maatwebsite\Excel\Concerns\ToArray;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Query\JoinClause;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
 use stdClass;
 
 class ProductosController extends Controller
@@ -45,18 +48,102 @@ class ProductosController extends Controller
         $object->success = true;
         $object->msg = "Products data";
         $object->items = $arrayResult;
-        //dd($object);
 
         return response()->json($object, 200, array('Content-Type' => 'application/json; charset=utf-8'));
     }
 
     public function getRepuesto($sku)
     {
-        $sku='01,01,001,240338-0001.jpg';
-        $asku=explode('.',$sku);
-        echo $asku[0]."\n";
-        echo $asku[1];
+        $asku=$this->f_transformarSku($sku);
+        $etcodlin=$asku[0];
+        $etcodori=$asku[1];
+        $etcodmar=$asku[2];
+        $etcodart=$asku[3];
+        $etsts='A';
+
+        $repuesto = DB::connection('ibmi')->table('LIBPRDDAT.MMETREL0')
+        ->where('ETCODLIN', '=', $etcodlin)
+        ->where('ETCODORI', '=', $etcodori)
+        ->where('ETCODMAR', '=', $etcodmar)
+        ->where('ETCODART', '=', $etcodart)
+        ->where('ETSTS', '=', $etsts)
+        ->limit(1)
+        ->get();
+
+        return response()->json($repuesto);
     }
 
+    private function f_transformarSku($sku)
+    {
+        $fsku=array();
+        $asku=explode('.',$sku);
+        $nsku=substr($asku[0],0,strlen($asku[0])-5);
+        $fsku=explode(',',$nsku);
+        return $fsku;
+    }
+
+    public function getMarca()
+    {
+        $eysts = 'A';
+
+        $marca_db2 = DB::connection('ibmi')->table('LIBPRDDAT.MMEYREL0')
+        ->select('EYCODMAR AS CODIGO', 'EYDSCLAR AS DESCRIPCION')
+        ->where('EYSTS', '=', $eysts)
+        ->orderBy('EYCODMAR', 'ASC')
+        ->get();
+
+        $marca = array();
+        foreach ($marca_db2 as $row) {
+            $row->codigo = utf8_encode(trim($row->codigo));
+            $row->descripcion = utf8_encode(trim($row->descripcion));
+            array_push($marca, $row);
+        }
+
+        return response()->json($marca);
+    }
+
+    public function getLinea()
+    {
+        $eusts = 'A';
+        $eucodtbl = '12';
+
+        $linea_db2 = DB::connection('ibmi')->table('LIBPRDDAT.MMEUREL0')
+        ->select('EUCODELE AS CODIGO', 'EUDSCLAR AS DESCRIPCION')
+        ->where('EUSTS', '=', $eusts)
+        ->where('EUCODTBL', '=', $eucodtbl)
+        ->orderBy('EUCODELE', 'ASC')
+        ->get();
+
+        $linea = array();
+        foreach ($linea_db2 as $row) {
+            $row->codigo = utf8_encode(trim($row->codigo));
+            $row->descripcion = utf8_encode(trim($row->descripcion));
+            array_push($linea, $row);
+        }
+
+        return response()->json($linea);
+    }
+
+    public function getOrigen()
+    {
+        $eusts = 'A';
+        $eucodtbl = '11';
+
+        $origen_db2 = DB::connection('ibmi')->table('LIBPRDDAT.MMEUREL0')
+        ->select('EUCODELE AS CODIGO', 'EUDSCLAR AS DESCRIPCION')
+        ->where('EUSTS', '=', $eusts)
+        ->where('EUCODTBL', '=', $eucodtbl)
+        ->orderBy('EUCODELE', 'ASC')
+        ->get();
+
+        $origen = array();
+        foreach ($origen_db2 as $row) {
+            $row->codigo = utf8_encode(trim($row->codigo));
+            $row->descripcion = utf8_encode(trim($row->descripcion));
+            array_push($origen, $row);
+        }
+
+        return response()->json($origen);
+    }
 
 }
